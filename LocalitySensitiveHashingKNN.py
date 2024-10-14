88cad8fcb85dc69670c3a87014db881869438f44
Yami <santox25@gmail.com>
2024-10-14 09:20:40 +0200

    lsh class

--- LocalitySensitiveHashingKNN.py
+++ LocalitySensitiveHashingKNN.py
@@ -16,12 +16,33 @@ def cosine_distance(a, b):
     return 1 - (a @ b)
 
 
-def knn_cosine(X_train, y_train, X_test, k=3):
+def knn_cosine(X_train, Y_train, X_test, k=3):
     predictions = []
     X_train = X_train / np.linalg.norm(X_train, axis=1, keepdims=True)
     X_test = X_test / np.linalg.norm(X_test, axis=1, keepdims=True)
     dists = 1 - np.dot(X_test, X_train.T)
-    closest = np.argsort(cos_dist, axis=1)[:, :k]
-    predictions = np.array([np.bincount(y_train[closest[i]]).argmax() for i in range(X_test.shape[0])])
+    closest = np.argsort(dists, axis=1)[:, :k]
+    predictions = np.array([np.bincount(Y_train[closest[i]]).argmax() for i in range(X_test.shape[0])])
 
     return predictions
+
+class LSH:
+    def __init__(self, num_hashes, num_buckets, input_dim):
+        self.num_hashes = num_hashes
+        self.num_buckets = num_buckets
+        self.hash_functions = [np.random.randn(input_dim) for _ in range(num_hashes)]
+        self.buckets = {}
+
+    def _hash(self, x):
+        return tuple((np.dot(x, h) > 0).astype(int) for h in self.hash_functions)
+
+    def fit(self, X, y):
+        for i, x in enumerate(X):
+            bucket_id = self._hash(x)
+            if bucket_id not in self.buckets:
+                self.buckets[bucket_id] = []
+            self.buckets[bucket_id].append((x, y[i]))
+
+    def query(self, x):
+        bucket_id = self._hash(x)
+        return self.buckets.get(bucket_id, [])
