8bb762ceac4105f88051e1e527351aa0cdad3f5a
Yami <santox25@gmail.com>
2024-10-31 23:35:25 +0100

    init assignment3

---
+++ Assignment3.py
@@ -0,0 +1,115 @@
+!pip install -q gdown httpimport
+
+# Standard IPython notebook imports
+%matplotlib inline
+
+import os
+from io import StringIO
+
+import graphviz
+import httpimport
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.stats as sstats
+import seaborn as sns
+import sklearn.ensemble
+import sklearn.tree
+from tqdm import tqdm_notebook
+
+# In this way we can import functions straight from gitlab
+with httpimport.gitlab_repo('SHassonaProjekt', 'inzynieria_obliczeniowa_23_24'):
+     from common.plotting import plot_mat
+
+sns.set_style("whitegrid")
+
+columns = [
+    "target",
+    "cap-shape",
+    "cap-surface",
+    "cap-color",
+    "bruises?",
+    "odor",
+    "gill-attachment",
+    "gill-spacing",
+    "gill-size",
+    "gill-color",
+    "stalk-shape",
+    "stalk-root",
+    "stalk-surface-above-ring",
+    "stalk-surface-below-ring",
+    "stalk-color-above-ring",
+    "stalk-color-below-ring",
+    "veil-type",
+    "veil-color",
+    "ring-number",
+    "ring-type",
+    "spore-print-color",
+    "population",
+    "habitat",
+]
+
+# Use read_csv to load the data.
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
+mushroom_df = pd.read_csv(url, header=None, names=columns)
+mushroom_idx_df = mushroom_df.reset_index()
+
+# 2. Iris
+iris_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+iris_df = pd.read_csv(
+    iris_url,
+    header=None,
+    names=["sepal_length", "sepal_width", "petal_length", "petal_width", "target"],
+)
+
+# 3. Congressoinal Voting
+# Binary attributes, binary class, missing data
+vote_df = (
+    pd.read_csv(
+        "https://pkgstore.datahub.io/machine-learning/vote/vote_csv/data/65f1736301dee4a2ad032abfe2a61acb/vote_csv.csv"
+    )
+    .rename({"Class": "target"}, axis=1)
+    .fillna("na")
+)
+
+# 4. Adult
+# census records, continuous and categorical attributes (some ordered), missing values
+adult_names = [
+    "Age",
+    "Workclass",
+    "fnlwgt",
+    "Education",
+    "Education-Num",
+    "Martial Status",
+    "Occupation",
+    "Relationship",
+    "Race",
+    "Sex",
+    "Capital Gain",
+    "Capital Loss",
+    "Hours per week",
+    "Country",
+    "target",
+]
+adult_df = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
+    names=adult_names,
+    header=None,
+    na_values="?",
+)
+adult_test_df = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test",
+    names=adult_names,
+    header=None,
+    na_values="?",
+    skiprows=1,
+)
+
+# 5. German Credit
+
+german_df = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data",
+    names=[f"A{d}" for d in range(1, 21)] + ["target"],
+    header=None,
+    sep=" ",
+)
