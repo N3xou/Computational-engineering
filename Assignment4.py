44235abbd2dc7caa77ce9c4e016128094f5148ad
Yami <santox25@gmail.com>
2024-11-15 00:45:03 +0100

    smallnet

--- Assignment4.py
+++ Assignment4.py
@@ -14,3 +14,64 @@ plt.scatter(
 plt.xlabel("X[0]")
 plt.ylabel("X[1]")
 plt.axis("square")
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class SmallNet:
+    def __init__(self, in_features, num_hidden, dtype=np.float32):
+        self.W1 = np.zeros((num_hidden, in_features), dtype=dtype)
+        self.b1 = np.zeros((num_hidden,), dtype=dtype)
+        self.W2 = np.zeros((1, num_hidden), dtype=dtype)
+        self.b2 = np.zeros((1,), dtype=dtype)
+        self.init_params()
+
+    def init_params(self):
+        self.W1 = np.random.normal(0, 0.5, self.W1.shape)
+        self.b1 = np.random.normal(0, 0.5, self.b1.shape)
+        self.W2 = np.random.normal(0, 0.5, self.W2.shape)
+        self.b2 = np.random.normal(0, 0.5, self.b2.shape)
+
+    def forward(self, X, Y=None, do_backward=False):
+        # TODO Problem 1: Fill in details of forward propagation
+
+        # Input to neurons in 1st layer
+        A1 = X @ self.W1.T + self.b1
+        # Outputs after the sigmoid non-linearity
+        O1 = sigmoid(A1)
+        # Inputs to neuron in the second layer
+        A2 = O1 @ self.W2.T + self.b2
+        # Outputs after the sigmoid non-linearity
+        O2 = sigmoid(A2)
+
+        # When Y is none, simply return the predictions. Else compute the loss
+        if Y is not None:
+            loss = -Y * np.log(O2) - (1 - Y) * np.log(1.0 - O2)
+            # normalize loss by batch size
+            loss = loss.sum() / X.shape[0]
+        else:
+            loss = np.nan
+
+        if do_backward:
+            # TODO in Problem 2:
+            # fill in the gradient computation
+            # Please note, that there is a correspondance between
+            # the forward and backward pass: with backward computations happening
+            # in reverse order.
+            # We save the gradients with respect to the parameters as fields of self.
+            # It is not very elegant, but simplifies training code later on.
+
+            # A2_grad is the gradient of loss with respect to A2
+            # Hint: there is a concise formula for the gradient
+            # of logistic sigmoid and cross-entropy loss
+            A2_grad = O2 - Y
+            self.b2_grad = A2_grad.sum(axis=0) / X.shape[0]
+            self.W2_grad = (A2_grad.T @ O1) / X.shape[0]
+            O1_grad = A2_grad @ self.W2 * O1 * (1 - O1)
+            A1_grad = O1_grad * O1 * (1 - O1)
+            self.b1_grad = O1_grad.sum(axis=0) / X.shape[0]
+            self.W1_grad = (O1_grad.T @ X) / X.shape[0]
+
+        return O2, loss
+
